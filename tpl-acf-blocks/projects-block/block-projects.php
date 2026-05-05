1c91a48a0526a3b4c7e96a037b502933e6f87626
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-05-05 22:52:42 +0200

    feat: add button to project cards

--- tpl-acf-blocks/projects-block/block-projects.php
+++ tpl-acf-blocks/projects-block/block-projects.php
@@ -47,6 +47,14 @@ $block = get_field( 'projects_block' ); ?>
                                     <?php echo wp_kses_post( $project->post_excerpt ); ?>
                                 </div>
                             <?php endif; ?>
+
+                            <a href="<?php echo get_permalink($project->ID)?>" class="project_card__button" aria-label="Read more about <?php echo esc_html( $project->post_title ); ?>">
+                                Read more
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.75 9H14.25" stroke="#021517" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M9 3.75L14.25 9L9 14.25" stroke="#021517" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 <?php endforeach; ?>
