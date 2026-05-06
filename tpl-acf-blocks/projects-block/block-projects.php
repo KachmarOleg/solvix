f4c10f5b474d1c3f547270960c19f361b356c5e7
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-05-06 20:16:55 +0200

    feat: enhance project card interactivity with hover and active effects

--- tpl-acf-blocks/projects-block/block-projects.php
+++ tpl-acf-blocks/projects-block/block-projects.php
@@ -27,36 +27,35 @@ $block = get_field( 'projects_block' ); ?>
             <?php if ( $projects = $block['projects'] ) : ?>
                 <?php foreach ( $projects as $project ) : ?>
                     <?php $img_id = get_post_thumbnail_id( $project->ID ); ?>
-                    <div class="project_card">
+
+                    <a href="<?php echo get_permalink($project->ID)?>" class="project_card" aria-label="Read more about <?php echo esc_html( $project->post_title ); ?>">
                         <?php if ( $img_id ) : ?>
-                            <figure class="project_card__image glass_effect">
-                                <a href="<?php echo get_permalink($project->ID)?>" tabindex="-1" aria-hidden="true">
-                                    <?php echo wp_get_attachment_image( $img_id, 'full', false, array( 'alt' => get_alt( $img_id ), 'class' => 'object_fit' ) ); ?>
-                                    <div class="glass-strips"></div>
-                                </a>
+                            <figure class="project_card__image">
+                                <?php echo wp_get_attachment_image( $img_id, 'full', false, array( 'alt' => get_alt( $img_id ), 'class' => 'object_fit' ) ); ?>
                             </figure>
                         <?php endif; ?>
 
                         <div class="project_card__content">
-                            <?php if ( $project->post_title ) : ?>
-                                <h3 class="project_card__title"><?php echo esc_html( $project->post_title ); ?></h3>
-                            <?php endif; ?>
+                            <div class="card_caption">
+                                <?php if ( $project->post_title ) : ?>
+                                    <h3 class="project_card__title"><?php echo esc_html( $project->post_title ); ?></h3>
+                                <?php endif; ?>
 
-                            <?php if ( $project->post_excerpt ) : ?>
-                                <div class="project_card__description content">
-                                    <?php echo wp_kses_post( $project->post_excerpt ); ?>
-                                </div>
-                            <?php endif; ?>
+                                <?php if ( $project->post_excerpt ) : ?>
+                                    <div class="project_card__description content">
+                                        <?php echo wp_kses_post( $project->post_excerpt ); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
 
-                            <a href="<?php echo get_permalink($project->ID)?>" class="project_card__button" aria-label="Read more about <?php echo esc_html( $project->post_title ); ?>">
-                                Read more
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <div class="project_card__button">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M3.75 9H14.25" stroke="#021517" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                     <path d="M9 3.75L14.25 9L9 14.25" stroke="#021517" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                 </svg>
-                            </a>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 <?php endforeach; ?>
             <?php endif; ?>
         </div>
