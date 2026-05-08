ede9cdcfcf24f463746bc192a9a4d5d24f2fa6b9
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-05-08 20:23:54 +0200

    feat: update top panel layout with responsive design and improved image handling

--- tpl-parts/top-panels/top-panel-secondary.php
+++ tpl-parts/top-panels/top-panel-secondary.php
@@ -12,7 +12,41 @@ $button_url = $top_panel['project_url'] ?? null;
 
     <div class="grid_decor"></div>
 
-    <div class="start_2_cols_3 text_col">
+
+    <div class="container rwd_show tablet_view">
+        <?php if ( has_post_thumbnail( get_the_ID() ) ) : ?>
+            <figure class="top_panel__image">
+                <?php echo wp_get_attachment_image( $thumb_id, 'full', false, array( 'alt' => get_alt( $thumb_id ), 'class' => 'object_fit' ) ); ?>
+            </figure>
+        <?php endif; ?>
+
+        <div class="text_col">
+            <?php if ( custom_tax($project->ID, 'industry') ) : ?>
+                <div class="subtitle"><?php echo custom_tax($project->ID, 'industry'); ?></div>
+            <?php endif; ?>
+
+            <h1><?php echo get_the_title(); ?></h1>
+
+            <?php if ( $project->post_excerpt ) : ?>
+                <div class="top_panel__description content">
+                    <?php echo wp_kses_post( $project->post_excerpt ); ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ( $button_url ) : ?>
+                <a href="<?php echo esc_url( $button_url ); ?>" class="project_button button" target="_blank">
+                    View Project
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.75 9H14.25" stroke="#021517" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M9 3.75L14.25 9L9 14.25" stroke="#021517" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+
+    <div class="start_2_cols_3 text_col rwd_hide">
         <?php if ( custom_tax($project->ID, 'industry') ) : ?>
             <div class="subtitle"><?php echo custom_tax($project->ID, 'industry'); ?></div>
         <?php endif; ?>
@@ -37,7 +71,7 @@ $button_url = $top_panel['project_url'] ?? null;
     </div>
 
     <?php if ( has_post_thumbnail( get_the_ID() ) ) : ?>
-        <figure>
+        <figure class="rwd_hide">
             <?php echo wp_get_attachment_image( $thumb_id, 'full', false, array( 'alt' => get_alt( $thumb_id ), 'class' => 'object_fit' ) ); ?>
         </figure>
     <?php endif; ?>
